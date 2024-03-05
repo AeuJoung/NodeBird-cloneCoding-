80d406b92e26f84155de8804522b8600fe39ed38
AEU <aeuplayground@gmail.com>
2024-03-05 23:14:42 +0900

    20240305

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,8 @@ export default function RootLayout({ //최상위 레이아웃
   return (
     <html lang="en">
       <body className={inter.className}>
-        루트 레이아웃  
-        {children}
+        <div className={styles.container}>{children}</div>
+        
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import styles from "./page.module.css"; // css module 쓸 것임. talewind 안씀.
+// tailwind -> 호불호 심함. 가독성 안좋음.
+//styled component or Emotion -> 이모션 next13에서 잘 안돌아감. styled 컴포넌트 SSR에서 문제있음
+// sass 
+// css module // 아래 styles.signup의 signup 부분이 클래스가 됨. (모듈 파일 가서 설정하면 됨)
+// vanila extract -> WINDOWS와 문제있음. 요즘 핫하긴 함.
+import zLogo from "../../public/zlogo.png";
 
 export default function Home() {
   return (
-    <div>
-      <div>
+    <>
+      <div className={styles.left}>
         <Image src={zLogo} alt="logo" />
       </div>
-      <div className="{styles.right}">
+      <div className={styles.right}> 
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요.</h2>
         <Link href="/i/flow/signup" className={styles.signup}>계정 만들기</Link>
@@ -15,6 +22,6 @@ export default function Home() {
         <Link href="/login" className={styles.login}>로그인</Link>
       
       </div>
-    </div>
+    </>
   );
 }
